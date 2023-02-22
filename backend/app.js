e134108c1035b39da55d70b8d009a61a01ce9b7a
Sayyed-rehan <rehansayyed1216@gmail.com>
2023-02-22 11:28:32 +0530

    nodemon added

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(cors())
 
 
 //mongodb
-mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URL)
 .then(()=>console.log('mongodb connected'))
 .catch((err)=>console.log(err));
